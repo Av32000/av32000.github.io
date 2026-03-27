ef08f5af1428bd41b81a89410bccdf5af9107a91
Av32000 <ae.av32000@gmail.com>
2026-03-27 18:04:28 +0100

    Update content for AboutMe and ITSkills

--- data/skills.js
+++ data/skills.js
@@ -3,8 +3,8 @@ const itSkills = [
     id: 0,
     name: "Git",
     content: [
-      "Complex and powerful, Git it's one of my favorite tools. I use it since a long time in every project I start. I've read and watched a lot of books and tutorials to get to know it inside out.",
-      "I'm not an expert but I know most of the important functions very well.",
+      "Complex and powerful, Git is one of my favorite tools. I use it since a long time in every project I start. I've read and watched a lot of books and tutorials to get to know it inside out.",
+      "I still learn new features but I know most of the important ones very well.",
     ],
   },
   {
@@ -12,39 +12,41 @@ const itSkills = [
     name: "Node.Js",
     content: [
       "I discovered Node.JS on Discord when I wanted to learn how to code bots.",
-      "Today, I use it in almost all my projects with libraries such as fastify for APIs, react for web, electron for software and so many others.",
-      "This framework allowed me to discover my great passion for the backend with Javascript.",
+      "Today, I use it in many projects with libraries such as fastify for APIs, react for web, electron for software and so many others.",
+      "This framework allowed me to discover my great passion for the backend technologies.",
     ],
   },
   {
     id: 2,
     name: "JavaScript",
     content: [
-      "My favorite language, both for its strange simplicity and for the way it works for everything (web, servers, software...).",
-      "Javascript lets me simply code applications that run in a terminal (with NodeJS), so I don't have to create a UI.",
+      "One of my favorite languages, both for its strange simplicity and for the way it works for everything (web, servers, software...).",
+      "I use JavaScript in many projects and have a strong knowledge of its ecosystem.",
     ],
   },
   {
     id: 3,
     name: "React",
     content: [
-      "Powerful and easy to use, React is my default framework for coding web interface. I use it whenever possible in all my projects and I prefer it to all others.",
+      "Powerful and easy to use, React is my default framework for coding web interfaces. I use it whenever possible in all my projects and I prefer it to all others.",
     ],
   },
   {
     id: 4,
     name: "Docker",
     content: [
-      "Docker is an essential tool for many companies. I use it regulary to learn how to use it in most cases as possible.",
+      "Docker is an essential tool for many companies. I use it regularly to learn how to use it in most cases as possible.",
       "Even if I'm not a pro, I know how it works and how to use it in the simplest cases.",
+      "I play a lot with it on my Homelab and like the container philosophy.",
     ],
   },
   {
     id: 5,
     name: "Rust",
     content: [
-      "Rust is a systems programming language that focuses on safety and performance. I started learning it to improve my understanding of low-level programming concepts.",
-      "I appreciate its modularity, allowing it to be used in many different projects, from web servers to command-line tools.",
+      "Rust is my favorite language. I started learning it to improve my understanding of low-level programming concepts.",
+      "Today I use it in almost all my new projects.",
+      "I appreciate its modularity, allowing it to be used in many different cases, from web servers to command-line tools.",
     ],
   },
   {
@@ -52,8 +54,8 @@ const itSkills = [
     name: "Linux",
     callback: () => applyOSMessage(),
     content: [
-      "As an open source fan, Linux is my favorite operating system. It has replaced Windows since many time on my computers.",
-      "The more I use it, the less I support Windows. No more untimely updates, unmodifiable system files, trackers and bloatware. With Linux, I can choose and customize every parts of my system to create an optimized environment.",
+      "As an open source fan, Linux is my favorite operating system. It has replaced Windows for a long time on my computers.",
+      "The more I use it, the less I support Windows. No more untimely updates, unmodifiable system files, trackers and bloatware. With Linux, I can choose and customize every part of my system to create an optimized environment.",
       '<span id="os-message"></span>',
     ],
   },
@@ -68,7 +70,7 @@ const itSkills = [
     id: 8,
     name: "GitHub",
     content: [
-      "Essential for almost every IT project Github it's the perfect tool for code and team management. I use it every day in all my projects and I'm always very happy with it.",
+      "Essential for almost every IT project Github is the perfect tool for code and team management. I use it every day in all my projects and I'm always very happy with it.",
       "It took me a while to learn how to use it perfectly, but it's one the best investment of time I've made.",
     ],
   },
@@ -77,7 +79,7 @@ const itSkills = [
     name: "Tailwind CSS",
     icon: "Tailwind",
     content: [
-      "Tailwind is my favorite css framework. It's very easy to use and include all css functionalities.",
+      "Tailwind is my favorite css framework. It's very easy to use and includes all CSS functionalities.",
       "It unifies the design while making development simpler and faster.",
     ],
   },
